878bfb30a710c26ba4306d531d7d48d8c452ef5c
cflem <carson.fl3ming@gmail.com>
2016-12-29 15:39:41 -0700

    Avoided die clause on guest accessing dash

--- dash/dash_common.php
+++ dash/dash_common.php
@@ -1,8 +1,7 @@
 <?php
 require('../inc/common.php');
+if ($b_user['id'] < 1) l_redirect('login.php');
 
 define('ACC_TYPE', $b_user['type']);
 require('nav.php');
-
-if ($b_user['id'] < 1) l_redirect('login.php');
 ?>
