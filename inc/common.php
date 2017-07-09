f70d8e35250a9a222eb32a4698cff42da95abf92
Carson Fleming <cflems@cflems.net>
2017-07-09 00:49:14 -0700

    Re-enabled mobile access

--- inc/common.php
+++ inc/common.php
@@ -3,7 +3,7 @@ define('INCLUDE_PATH', dirname(__FILE__).'/');
 
 require(INCLUDE_PATH . 'config.php');
 require(INCLUDE_PATH . 'functions.php');
-require(INCLUDE_PATH . 'mobile.php');
+//require(INCLUDE_PATH . 'mobile.php');
 require(INCLUDE_PATH . 'db.php');
 require(INCLUDE_PATH . 'user.php');
 require(INCLUDE_PATH . 'mailgun/autoload.php');
